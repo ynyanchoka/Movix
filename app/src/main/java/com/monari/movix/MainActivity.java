cbb58b89eb60b9e8a9585a95c0bad3efa4680219
ynyanchoka <monaryymelda@gmail.com>
2022-05-27 18:30:27 +0300

    add main activity.xml

--- app/src/main/java/com/monari/movix/MainActivity.java
+++ app/src/main/java/com/monari/movix/MainActivity.java
@@ -3,12 +3,25 @@ package com.monari.movix;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private Button mGetStartedButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        super.onCreate(savedInstanceState);//run all of the default behaviors for an activity
+        setContentView(R.layout.activity_main);//tells the activity which layout to use for the device screen
+
+        mGetStartedButton = (Button) findViewById(R.id.getStartedButton);//(Button) typecasts our view as Button
+        mGetStartedButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Toast.makeText(MainActivity.this, "Welcome to Movix", Toast.LENGTH_LONG).show();
+
+            }
+        });
     }
 }
