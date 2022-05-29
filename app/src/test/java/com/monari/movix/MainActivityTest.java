ed989b1c8e0f0444edd606be1dc430d6f5784800
ynyanchoka <monaryymelda@gmail.com>
2022-05-29 14:05:49 +0300

    add main activity local test

---
+++ app/src/test/java/com/monari/movix/MainActivityTest.java
@@ -0,0 +1,46 @@
+package com.monari.movix;
+
+import static org.junit.Assert.assertTrue;
+
+import android.content.Intent;
+import android.widget.TextView;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.shadows.ShadowActivity;
+
+@RunWith(RobolectricTestRunner.class)
+public class MainActivityTest {
+    //configure our test class so it knows which Activity we will use to write (and assert) our tests:
+        private MainActivity activity;
+        @Before
+        public void setUp() throws Exception {
+            activity = Robolectric.buildActivity(MainActivity.class)
+                    .create()
+                    .resume()
+                    .get();
+        }
+
+    @Test
+    public void secondActivityStarted(){
+        activity.findViewById(R.id.loginButton).performClick();
+        Intent expectedIntent = new Intent(activity, LoginActivity.class);
+        ShadowActivity shadowActivity = org.robolectric.Shadows.shadowOf(activity);
+        Intent actualIntent = shadowActivity.getNextStartedActivity();
+        assertTrue(actualIntent.filterEquals(expectedIntent));
+    }
+
+    @Test
+    public void anotherActivityStarted(){
+        activity.findViewById(R.id.signUpButton).performClick();
+        Intent expectedIntent = new Intent(activity, SignupActivity.class);
+        ShadowActivity shadowActivity = org.robolectric.Shadows.shadowOf(activity);
+        Intent actualIntent = shadowActivity.getNextStartedActivity();
+        assertTrue(actualIntent.filterEquals(expectedIntent));
+    }
+
+
+}
