1408de6484623ab0e7a3c02e1b0b16877a3540c6
ynyanchoka <monaryymelda@gmail.com>
2022-06-01 11:51:23 +0300

    add ApiKey

---
+++ app/src/main/java/com/monari/movix/Constants.java
@@ -0,0 +1,6 @@
+package com.monari.movix;
+
+public class Constants {
+    public static final String TMDB_BASE_URL = "https://api.themoviedb.org/3/";
+    public static final String  TMDB_API_KEY = BuildConfig.TMDB_API_KEY;
+}
